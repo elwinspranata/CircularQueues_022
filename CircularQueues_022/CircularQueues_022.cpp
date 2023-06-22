0d976b113e0d23c74bacbc0027f9db3356d08a0a
elwinspranata <elwinpranata02@gmail.com>
2023-06-22 11:28:35 +0700

    switch

--- CircularQueues_022/CircularQueues_022.cpp
+++ CircularQueues_022/CircularQueues_022.cpp
@@ -114,6 +114,26 @@ public:
 				cin >> ch; 
 				cout << endl; 
 
+				switch (ch) {
+				case '1': {
+					q.insert();
+					break;
+				} 
+				case '2': {
+					q.remove();
+					break;
+				} 
+				case '3': {
+					q.display();
+					break;
+				} 
+				case '4': {
+					return;
+
+
+				} 
+
+			
 
 			}
 		}
