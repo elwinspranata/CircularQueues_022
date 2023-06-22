d9503bc323d5e65cf68c80e0c1b8c32d2391ccda
elwinspranata <elwinpranata02@gmail.com>
2023-06-22 10:55:20 +0700

    cek jika antrian hanya memiliki satu elemen

--- CircularQueues_022/CircularQueues_022.cpp
+++ CircularQueues_022/CircularQueues_022.cpp
@@ -44,6 +44,13 @@ public:
 			cout << "Queue underflow\n";
 			return;
 		} 
+		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n"; 
+
+		// cek jika antrian hanya memiliki satu elemen 
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		} 
 
 	}
 
