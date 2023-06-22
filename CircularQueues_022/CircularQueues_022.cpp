c4bfd66926c10216ebea6aa33457e5cbfb5261ea
elwinspranata <elwinpranata02@gmail.com>
2023-06-22 10:57:22 +0700

    jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array

--- CircularQueues_022/CircularQueues_022.cpp
+++ CircularQueues_022/CircularQueues_022.cpp
@@ -43,15 +43,23 @@ public:
 		if (FRONT == -1) {
 			cout << "Queue underflow\n";
 			return;
-		} 
-		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n"; 
+		}
+		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 
 		// cek jika antrian hanya memiliki satu elemen 
 		if (FRONT == REAR) {
 			FRONT = -1;
 			REAR = -1;
-		} 
+		}
+		else {
+			// jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array 
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
+	} 
 
-	}
+
 
 };
